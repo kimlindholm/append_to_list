1bf530c4d636b532670668978f265e83a85e7f50
Kim Lindholm <misc@dynevision.com>
2019-03-22 00:16:37 +0700

    Run "mix new append_to_list"

---
+++ lib/append_to_list.ex
@@ -0,0 +1,18 @@
+defmodule AppendToList do
+  @moduledoc """
+  Documentation for AppendToList.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AppendToList.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/append_to_list_test.exs
@@ -0,0 +1,8 @@
+defmodule AppendToListTest do
+  use ExUnit.Case
+  doctest AppendToList
+
+  test "greets the world" do
+    assert AppendToList.hello() == :world
+  end
+end
