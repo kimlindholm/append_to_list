3644d472e18a12bbfc11c59d3add9b6301b36861
Kim Lindholm <misc@dynevision.com>
2019-03-23 13:01:27 +0700

    Add example of list comprehension that fails
    $ mix test
    
      1) doctest Example.build_activities/2 (4) (ExampleTest)
         Doctest failed
         code:  Example.build_activities([%{state: :error}], 2) === [%{state: :error}, %{state: :ok}, %{state: :ok}]
         left:  [[%{state: :error} | %{state: :ok}], [%{state: :error} | %{state: :ok}]]
         right: [%{state: :error}, %{state: :ok}, %{state: :ok}]

--- lib/append_to_list.ex
+++ lib/append_to_list.ex
@@ -26,6 +26,24 @@ defmodule Example do
     end
   end
 
+  @doc """
+  Build given number of activities and append to a list.
+
+  ## Examples
+
+      iex> Example.build_activities([], 2)
+      [%{state: :ok}, %{state: :ok}]
+
+      iex> Example.build_activities([%{state: :error}], 2)
+      [%{state: :error}, %{state: :ok}, %{state: :ok}]
+
+  """
+  def build_activities(activities, count) do
+    for _i <- 1..count do
+      activities ++ build_success_activity()
+    end
+  end
+
   defp build_success_activity do
     %{state: :ok}
   end
