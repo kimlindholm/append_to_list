4270c656a3a44ad4c2f90fa802e03875df66684c
Kim Lindholm <misc@dynevision.com>
2019-03-23 13:01:26 +0700

    Add first example and verify with "mix test"

--- lib/append_to_list.ex
+++ lib/append_to_list.ex
@@ -1,18 +1,32 @@
 defmodule Example do
   @moduledoc """
-  Documentation for Example.
+  Example of `Enum.reduce/3` vs. list comprehensions when pushing
+  maps to a list.
   """
 
   @doc """
-  Hello world.
+  Build given number of activities.
 
   ## Examples
 
-      iex> Example.hello()
-      :world
+      iex> Example.build_activities(3)
+      [%{state: :ok}, %{state: :ok}, %{state: :ok}]
+
+      iex> Example.build_activities(0)
+      []
 
   """
-  def hello do
-    :world
+  def build_activities(count) when count < 1, do: []
+
+  def build_activities(count) do
+    activities = []
+
+    for _i <- 1..count do
+      activities ++ build_success_activity()
+    end
+  end
+
+  defp build_success_activity do
+    %{state: :ok}
   end
 end
